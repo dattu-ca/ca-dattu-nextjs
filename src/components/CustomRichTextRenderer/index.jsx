b88925a20c90dd320773e25383e9dc0c863afe66
dattu-ca <97037036+dattu-ca@users.noreply.github.com>
2023-07-13 06:45:43 -0400

    Dev/185549226 (#4)
    * 185549226 Refactored out of typescript
    
    * 185549226 added comment to consol.log

--- src/components/CustomRichTextRenderer/index.jsx
+++ src/components/CustomRichTextRenderer/index.jsx
@@ -14,7 +14,7 @@ const options = {
     renderNode: {
         [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
         [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
-            console.log(JSON.stringify(node.data.target.sys.contentType.sys.id, null, 2))
+            // console.log(JSON.stringify(node.data.target.sys.contentType.sys.id, null, 2))
             return documentToReactComponents(node.data.target.fields.body, options)
         },
     },
