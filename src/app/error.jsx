d871141d7f84b1f71f3e2f8407c666f21b2c9f0f
dattu.ca <dattu.ca.website@gmail.com>
2023-07-13 06:43:36 -0400

    185549226 Refactored out of typescript

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -5,10 +5,7 @@ import {useEffect} from 'react';
 export default function Error({
                                   error,
                                   reset,
-                              }: {
-    error: Error
-    reset: () => void
-}) {
+                              }) {
     useEffect(() => {
         // Log the error to an error reporting service
         console.error(error)
