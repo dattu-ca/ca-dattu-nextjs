9840f329f80d8c5bee5254940985c4170986cabb
dattu.ca <dattu.ca.website@gmail.com>
2023-07-25 18:26:34 -0400

    185692785 added more marks/blocks

--- src/components/CustomRichTextRenderer/index.jsx
+++ src/components/CustomRichTextRenderer/index.jsx
@@ -4,16 +4,28 @@ import { BLOCKS, MARKS } from '@contentful/rich-text-types';
 
 
 const Bold = ({ children }) => <span className="font-bold">{children}</span>;
+const Italic = ({ children }) => <span className="italic">{children}</span>;
+const Underline = ({ children }) => <span className="underline">{children}</span>;
 const Text = ({ children }) => <p>{children}</p>;
+const Ol = ({ children }) => <ol className="">{children}</ol>;
+const Ul = ({ children }) => <ul className="">{children}</ul>;
+const Li = ({ children }) => <li className="">{children}</li>;
 
 
 const options = {
     renderMark: {
         [MARKS.BOLD]: (text) => <Bold>{text}</Bold>,
+        [MARKS.ITALIC]: (text) => <Italic>{text}</Italic>,
+        [MARKS.UNDERLINE]: (text) => <Underline>{text}</Underline>,
+        [MARKS.CODE]: (text) => <code>{text}</code>,
     },
     renderNode: {
         [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+        [BLOCKS.OL_LIST]: (node, children) => <Ol>{children}</Ol>,
+        [BLOCKS.UL_LIST]: (node, children) => <Ul>{children}</Ul>,
+        [BLOCKS.LIST_ITEM]: (node, children) => <Li>{children}</Li>,
         [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
+            const embeddedType = node.data.target.sys.contentType.sys.id;
             return documentToReactComponents(node.data.target.fields.body, options)
         },
     },
