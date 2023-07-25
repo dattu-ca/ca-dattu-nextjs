748011faa52113d59cb91766f643ad85371ecde0
dattu-ca <97037036+dattu-ca@users.noreply.github.com>
2023-07-25 16:35:03 -0400

    185682332 refactoring ts (#9)

--- src/app/pages/[slug]/interfaces.ts
+++
@@ -1,7 +0,0 @@
-
-export interface iProps {
-    params: {
-        slug: string;
-        slug: String;
-    }
-}
---
+++ src/app/pages/[slug]/types.ts
@@ -0,0 +1,6 @@
+
+export interface IProps {
+    params: {
+        slug: string;
+    }
+}
