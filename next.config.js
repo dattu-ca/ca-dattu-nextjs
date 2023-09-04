4a65aee3e4adddbaec3a1c592f4d6a1dfda518f5
dattu.ca <dattu.ca.website@gmail.com>
2023-09-04 10:13:16 -0400

    185963420 Added fix for reloading issue

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     experimental: {
+        appDir: true,
         swcPlugins: [["next-superjson-plugin", {}]],
     },
 };
-module.exports = nextConfig
