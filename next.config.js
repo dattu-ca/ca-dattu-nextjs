c2f0a7163ef94d4650918b765225a51828b5238f
dattu.ca <dattu.ca.website@gmail.com>
2023-09-04 09:38:33 -0400

    185963420 resetting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     experimental: {
-        appDir: true,
         swcPlugins: [["next-superjson-plugin", {}]],
     },
 };
