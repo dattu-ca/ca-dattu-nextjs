eef84132c02dcc1a6d590021fba8e62bf5f9a0ea
dattu.ca <dattu.ca.website@gmail.com>
2023-09-05 23:52:37 -0400

    185915197 Added the tagged template sanitize function

---
+++ src/utils/utils.ts
@@ -0,0 +1,30 @@
+
+const sanitize = (strings: TemplateStringsArray, ...values: Array<string | number>) => {
+    const sanitizedValues = values.map((value) =>
+        // Replacing the common XSS attack characters
+        String(value).replace(/[&<>"']/g, (char) => {
+            switch (char) {
+                case '&':
+                    return '&amp;';
+                case '<':
+                    return '&lt;';
+                case '>':
+                    return '&gt;';
+                case '"':
+                    return '&quot;';
+                case "'":
+                    return '&#39;';
+                default:
+                    return char;
+            }
+        })
+    );
+
+    return strings.reduce((result, string, i) => {
+        return result + string + (sanitizedValues[i] || '');
+    }, '');
+};
+
+export {
+    sanitize
+}
