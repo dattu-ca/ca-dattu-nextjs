82bff254265c9be567aafea6e2a6002a4cdadfad
dattu.ca <dattu.ca.website@gmail.com>
2023-09-08 16:43:03 -0400

    185979612 Fixing build errors

--- src/components/Banner/renderedImage.tsx
+++ src/components/Banner/renderedImage.tsx
@@ -1,5 +1,3 @@
-import clsx from "clsx";
-import Image from "next/image";
 import {IBodyImage} from "~/models";
 import {useMemo} from "react";
 
@@ -11,24 +9,24 @@ const RenderedImage = ({banner}: IProps) => {
 
     const srcSet = useMemo(() => {
         const srcSet = [];
-        if (banner.mobileImage) {
-            srcSet.push(`${banner.mobileImage.url}?fm=avif&w=640&q=75 640w`)
-        } else if ("url" in banner.desktopImage) {
-            srcSet.push(`${banner.desktopImage.url}?fm=avif&w=640&q=75 640w`)
-        }
-        if ("url" in banner.desktopImage) {
+        if(banner){
+            if (banner.mobileImage) {
+                srcSet.push(`${banner.mobileImage?.url}?fm=avif&w=640&q=75 640w`)
+            } else{
+                srcSet.push(`${banner.desktopImage?.url}?fm=avif&w=640&q=75 640w`)
+            }
             const sizes = [750, 828, 1080, 1200, 1920, 2048, 3840]
             sizes.forEach(size => {
-                if ("url" in banner.desktopImage) {
-                    srcSet.push(`${banner.desktopImage.url}?fm=avif&w=${size}&q=75 ${size}w`)
-                }
+                srcSet.push(`${banner.desktopImage?.url}?fm=avif&w=${size}&q=75 ${size}w`)
             })
         }
+        
         return srcSet;
-    }, [banner])
+    }, [banner]);
 
-    return <img alt={banner.desktopImage?.alt} 
-                loading="lazy" width="400"
+    return <img alt={banner?.desktopImage?.alt} 
+                loading="lazy" 
+                width="400"
                 height="400"
                 decoding="async" 
                 data-nimg="1" 
@@ -72,3 +70,30 @@ export {
  src="w=3840&amp;q=75">
 
  */
+
+
+
+// return (
+//     <picture className={clsx(
+//         'transition-all',
+//         'block w-auto h-[30px]',
+//         classes.flip
+//     )}>
+//         <source
+//             media="(max-width: 768px)"
+//             srcSet={`${logo?.mobileImage?.url} 768w`}
+//             sizes="768px"
+//             className='w-auto h-[40px]'
+//         />
+//         <source
+//             srcSet={`${logo?.desktopImage?.url} 1280w`}
+//             sizes="1280px"
+//         />
+//         <img src={logo?.desktopImage?.url}
+//              alt={logo?.desktopImage?.alt}
+//              className='h-[40px] max-w-none w-auto'
+//              width={40}
+//              height={40}
+//         />
+//     </picture>
+// )
--- src/components/Navbar/logo.tsx
+++ src/components/Navbar/logo.tsx
@@ -10,23 +10,21 @@ const Logo = () => {
 
     const srcSet = useMemo(() => {
         const srcSet = [];
-        if (logo.mobileImage) {
-            srcSet.push(`${logo.mobileImage.url}?fm=avif&w=40&q=75 640w`)
-        } else if ("url" in logo.desktopImage) {
-            srcSet.push(`${logo.desktopImage.url}?fm=avif&w=40&q=75 640w`)
-        }
-        if ("url" in logo.desktopImage) {
+        if(logo){
+            if (logo.mobileImage) {
+                srcSet.push(`${logo.mobileImage?.url}?fm=avif&w=40&q=75 640w`)
+            } else{
+                srcSet.push(`${logo.desktopImage?.url}?fm=avif&w=40&q=75 640w`)
+            }
             const sizes = [750, 828, 1080, 1200, 1920, 2048, 3840]
             sizes.forEach(size => {
-                if ("url" in logo.desktopImage) {
-                    srcSet.push(`${logo.desktopImage.url}?fm=avif&w=40&q=75 ${size}w`)
-                }
+                srcSet.push(`${logo.desktopImage?.url}?fm=avif&w=40&q=75 ${size}w`)
             })
         }
         return srcSet;
     }, [logo])
 
-    return <img alt={logo.desktopImage?.alt}
+    return <img alt={logo?.desktopImage?.alt}
                 loading="lazy" 
                 width="40"
                 height="40"
@@ -40,31 +38,6 @@ const Logo = () => {
                 sizes="40px"
                 srcSet={srcSet.join(', ')}
                 src={srcSet[srcSet.length - 1]}/>
-
-    // return (
-    //     <picture className={clsx(
-    //         'transition-all',
-    //         'block w-auto h-[30px]',
-    //         classes.flip
-    //     )}>
-    //         <source
-    //             media="(max-width: 768px)"
-    //             srcSet={`${logo?.mobileImage?.url} 768w`}
-    //             sizes="768px"
-    //             className='w-auto h-[40px]'
-    //         />
-    //         <source
-    //             srcSet={`${logo?.desktopImage?.url} 1280w`}
-    //             sizes="1280px"
-    //         />
-    //         <img src={logo?.desktopImage?.url}
-    //              alt={logo?.desktopImage?.alt}
-    //              className='h-[40px] max-w-none w-auto'
-    //              width={40}
-    //              height={40}
-    //         />
-    //     </picture>
-    // )
 }
 
 export {Logo};
