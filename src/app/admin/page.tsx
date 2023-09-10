9055a691ebf01f63fc660648184dbe16f168c6cf
dattu.ca <dattu.ca.website@gmail.com>
2023-09-10 19:39:34 -0400

    186006639

--- src/app/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-import {redirect} from 'next/navigation';
-import {RedirectType} from "next/dist/client/components/redirect";
-
-const Page = async () => {
-    redirect('/admin/dashboard', RedirectType.replace);
-}
-
-export default Page;
--- src/app/auth/page.tsx
+++
@@ -1,8 +0,0 @@
-import {redirect} from 'next/navigation';
-import {RedirectType} from "next/dist/client/components/redirect";
-
-const Page = async () => {
-    redirect('/auth/login', RedirectType.replace);
-}
-
-export default Page;
