460341c91ff7d8f04b538ccf5426cf137ec9d236
dattu.ca <dattu.ca.website@gmail.com>
2023-09-10 12:42:25 -0400

    186006639 Starting to add pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,15 @@
+import {NextRequest, NextResponse} from "next/server";
 
-export { default } from 'next-auth/middleware';
+export {default} from 'next-auth/middleware';
 
 export const config = {
     matcher: [
-        '/dashboard',
-        '/profile'
+        '/dashboard/:path*',
+        '/profile/:path*'
     ]
+}
+
+export const middleware = (request: NextRequest) => {
+    
+    return NextResponse.next();
 }
