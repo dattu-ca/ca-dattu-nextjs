882bfafbd7969a01e436229477c07bc90e870e5d
dattu.ca <dattu.ca.website@gmail.com>
2023-09-09 20:07:57 -0400

    185915196 Further attempts

--- src/components/ReactIcon/index.jsx
+++ src/components/ReactIcon/index.jsx
@@ -1,223 +1,177 @@
 import React, {Suspense} from 'react';
 import dynamic from 'next/dynamic'
 import {AiOutlineLoading3Quarters} from 'react-icons/ai';
-import {FiActivity} from 'react-icons/fi';
-import {GrAchievement} from 'react-icons/gr';
-import {RxActivityLog} from 'react-icons/rx'
-import clsx from "clsx";
-
-
-// const DynamicIcon2 = (icon, className) => dynamic(async () => {
-//     const match = /[A-Z0-9]/.exec(icon[0].toLowerCase() + icon.slice(1));
-//     const libStr = match ? icon.substring(0, match.index) : null;
-//     const Xyz = () => <pre>{JSON.stringify({match, icon, className, libStr}, null, 2)}</pre>
-//     return Xyz;
-//     if (match) {
-//        
-//         switch (libStr) {
-//             case 'Ai' :
-//                 const {[icon]: Ai} = await import('react-icons/ai');
-//                 if (Ai) {
-//                     return {default: Ai}
-//                 }
-//                 break;
-//             case 'Bs' :
-//                 const {[icon]: Bs} = await import('react-icons/ai');
-//                 if (Bs) {
-//                     return {default: Bs}
-//                 }
-//                 break;
-//             case 'Bi' :
-//                 const {[icon]: Bi} = await import('react-icons/bi');
-//                 if (Bi) {
-//                     return {default: Bi}
-//                 }
-//                 break;
-//             case 'Cg' :
-//                 const {[icon]: Cg} = await import('react-icons/cg');
-//                 if (Cg) {
-//                     return {default: Cg}
-//                 }
-//                 break;
-//             case 'Ci' :
-//                 const {[icon]: Ci} = await import('react-icons/ci');
-//                 if (Ci) {
-//                     return {default: Ci}
-//                 }
-//                 break;
-//             case 'Di' :
-//                 const {[icon]: Di} = await import('react-icons/di');
-//                 if (Di) {
-//                     return {default: Di}
-//                 }
-//                 break;
-//             case 'Fi' :
-//                 const {[icon]: Fi} = await import('react-icons/fi');
-//                 if (Fi) {
-//                     return {default: Fi}
-//                 }
-//                 break;
-//             case 'Fc' :
-//                 const {[icon]: Fc} = await import('react-icons/fc');
-//                 if (Fc) {
-//                     return {default: Fc}
-//                 }
-//                 break;
-//             case 'Fa' :
-//                 const {[icon]: Fa} = await import('react-icons/fa6');
-//                 console.log('FA', Fa);
-//                 if (Fa) {
-//                     return {default: Fa}
-//                 }
-//                 break;
-//             case 'Gi' :
-//                 const {[icon]: Gi} = await import('react-icons/gi');
-//                 if (Gi) {
-//                     return {default: Gi}
-//                 }
-//                 break;
-//             case 'Go' :
-//                 const {[icon]: Go} = await import('react-icons/go');
-//                 if (Go) {
-//                     return {default: Go}
-//                 }
-//                 break;
-//             case 'Gr' :
-//                 const {[icon]: Gr} = await import('react-icons/gr');
-//                 if (Gr) {
-//                     return {default: Gr}
-//                 }
-//                 break;
-//             case 'Hi' :
-//                 const {[icon]: Hi} = await import('react-icons/hi2');
-//                 if (Hi) {
-//                     return {default: Hi}
-//                 }
-//                 break;
-//             case 'Im' :
-//                 const {[icon]: Im} = await import('react-icons/im');
-//                 if (Im) {
-//                     return {default: Im}
-//                 }
-//                 break;
-//             case 'Lia' :
-//                 const {[icon]: Lia} = await import('react-icons/lia');
-//                 if (Lia) {
-//                     return {default: Lia}
-//                 }
-//                 break;
-//             case 'Io' :
-//                 const {[icon]: Io} = await import('react-icons/io5');
-//                 if (Io) {
-//                     return {default: Io}
-//                 }
-//                 break;
-//             case 'Lu' :
-//                 const {[icon]: Lu} = await import('react-icons/lu');
-//                 if (Lu) {
-//                     return {default: Lu}
-//                 }
-//                 break;
-//             case 'Md' :
-//                 const {[icon]: Md} = await import('react-icons/md');
-//                 if (Md) {
-//                     return {default: Md}
-//                 }
-//                 break;
-//             case 'Pi' :
-//                 const {[icon]: Pi} = await import('react-icons/pi');
-//                 if (Pi) {
-//                     return {default: Pi}
-//                 }
-//                 break;
-//             case 'Ri' :
-//                 const {[icon]: Ri} = await import('react-icons/ri');
-//                 if (Ri) {
-//                     return {default: Ri}
-//                 }
-//                 break;
-//             case 'Rx' :
-//                 const {[icon]: Rx} = await import('react-icons/rx');
-//                 if (Rx) {
-//                     return {default: Rx}
-//                 }
-//                 break;
-//             case 'Si' :
-//                 const {[icon]: Si} = await import('react-icons/si');
-//                 if (Si) {
-//                     return {default: Si}
-//                 }
-//                 break;
-//             case 'Sl' :
-//                 const {[icon]: Sl} = await import('react-icons/sl');
-//                 if (Sl) {
-//                     return {default: Sl}
-//                 }
-//                 break;
-//             case 'Tb' :
-//                 const {[icon]: Tb} = await import('react-icons/tb');
-//                 if (Tb) {
-//                     return {default: Tb}
-//                 }
-//                 break;
-//             case 'Tfi' :
-//                 const {[icon]: Tfi} = await import('react-icons/tfi');
-//                 if (Tfi) {
-//                     return {default: Tfi}
-//                 }
-//                 break;
-//             case 'Ti' :
-//                 const {[icon]: Ti} = await import('react-icons/ti');
-//                 if (Ti) {
-//                     return {default: Ti}
-//                 }
-//                 break;
-//             case 'Vsc' :
-//                 const {[icon]: Vsc} = await import('react-icons/vsc');
-//                 if (Vsc) {
-//                     return {default: Vsc}
-//                 }
-//                 break;
-//             case 'Wi' :
-//                 const {[icon]: Wi} = await import('react-icons/wi');
-//                 if (Wi) {
-//                     return {default: Wi}
-//                 }
-//                 break;
-//             default:
-//                 break;
-//         }
-//     }
-//     return {
-//         default: AiOutlineLoading3Quarters
-//     }
-// }, {
-//     loading: () => <FiActivity className={clsx(
-//         className,
-//         'animate-spin'
-//     )}/>,
-//     ssr: false
-// })
-
+// import {FiActivity} from 'react-icons/fi';
+// import {GrAchievement} from 'react-icons/gr';
+// import {RxActivityLog} from 'react-icons/rx'
+// import clsx from "clsx";
 
 
 const ReactIcon = ({icon, className}) => {
 
 
-    const DynamicIcon = dynamic( () => {
-        const i ='AiFillAlert'
-        return import('react-icons/ai').then(mod => mod[i]);
+    
+    const DynamicIcon = dynamic(() => {
+        const match = /[A-Z0-9]/.exec(icon[0].toLowerCase() + icon.slice(1));
+        const libStr = match ? icon.substring(0, match.index) : null;
+        if (libStr) {
+            const i = icon;
+            switch (libStr) {
+                case 'Ai' :
+                    return import('react-icons/ai').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Bs' :
+                    return import('react-icons/bs').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Bi' :
+                    return import('react-icons/bi').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Cg' :
+                    return import('react-icons/cg').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Ci' :
+                    return import('react-icons/ci').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Di' :
+                    return import('react-icons/di').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Fi' :
+                    return import('react-icons/fi').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Fc' :
+                    return import('react-icons/fc').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Fa' :
+                    return import('react-icons/fa6').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Gi' :
+                    return import('react-icons/gi').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Go' :
+                    return import('react-icons/go').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Gr' :
+                    return import('react-icons/gr').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Hi' :
+                    return import('react-icons/hi2').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Im' :
+                    return import('react-icons/im').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Lia' :
+                    return import('react-icons/lia').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Io' :
+                    return import('react-icons/io5').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Lu' :
+                    return import('react-icons/lu').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Md' :
+                    return import('react-icons/md').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Pi' :
+                    return import('react-icons/pi').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Ri' :
+                    return import('react-icons/ri').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Rx' :
+                    return import('react-icons/rx').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Si' :
+                    return import('react-icons/si').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Sl' :
+                    return import('react-icons/sl').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Tb' :
+                    return import('react-icons/tb').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Tfi' :
+                    return import('react-icons/tfi').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Ti' :
+                    return import('react-icons/ti').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Vsc' :
+                    return import('react-icons/vsc').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                case 'Wi' :
+                    return import('react-icons/wi').then(mod => {
+                        const Icon = mod[i];
+                        return () => <Icon className={className}/>
+                    });
+                default:
+                    return import('react-icons/ai').then(mod => {
+                        const Icon = mod.AiOutlineLoading3Quarters;
+                        return () => <Icon className={className}/>
+                    });
+            }
+        }
+    }, {
+        loading: () => <p>loading...</p>
     })
-    
+
     const Icon = DynamicIcon(icon, className);
-    
-    
-    if (Icon) {
-        return <Suspense fallback={<p>...Sus Main</p>}>
-            {Icon}
-        </Suspense>
-    }
-    return <RxActivityLog className={clsx(className, 'Final-Return')}/>;
+    return <Suspense fallback={<p>...Sus Main</p>}>
+        {Icon}
+    </Suspense>
 }
 
 export {ReactIcon}
