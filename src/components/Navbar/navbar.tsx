095fb003a074540a6d583b8da8200500fafd5872
dattu.ca <dattu.ca.website@gmail.com>
2023-09-26 10:57:36 -0400

    186121525 Fixing a strange div in div bug

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,22 +1,24 @@
 'use client'
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import {useBreakpoint} from "~/hooks/useBreakpoint";
 import {NavbarDesktop} from "./desktop";
 import {NavbarMobile} from "./mobile";
+import clsx from "clsx";
 
 
 const Navbar = () => {
     const {isAboveSm} = useBreakpoint("sm");
+    
+    const [isDesktop, setIsDesktop] = useState(false)
+    
+    useEffect(() => {
+        setIsDesktop(isAboveSm);
+    }, [isAboveSm]);
+    
     return (
         <Fragment>
-            {
-                isAboveSm
-                && <NavbarDesktop/>
-            }
-            {
-                !isAboveSm
-                && <NavbarMobile/>
-            }
+            <div className={clsx('hidden md:block')}>{isDesktop && <NavbarDesktop/>}</div>
+            <div className={clsx('block md:hidden')}>{!isDesktop && <NavbarMobile/>}</div>
         </Fragment>
     )
 };
