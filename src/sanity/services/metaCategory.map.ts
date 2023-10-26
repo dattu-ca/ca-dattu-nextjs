614487219a8c4ef6cd81b1632dd9110601df35c3
dattu.ca <dattu.ca.website@gmail.com>
2023-10-25 23:40:21 -0400

    186338674

--- src/sanity/services/metaCategory.map.ts
+++ src/sanity/services/metaCategory.map.ts
@@ -4,9 +4,9 @@ export const mapSanity = (raw: any) => {
     const target: Partial<MetaCategory> = {
         cmsSource: 'Sanity',
         contentType: 'MetaCategory',
-        sysId: raw.sysId as string,
-        slug: raw.slug as string,
-        name: raw.name as string,
+        sysId: raw?.sysId as string,
+        slug: raw?.slug as string,
+        name: raw?.name as string,
     }
     return target as MetaCategory;
 }
