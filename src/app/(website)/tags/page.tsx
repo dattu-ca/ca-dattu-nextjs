9a303cf8640f5999d3aff44a38a4bf13b7a96edb
dattu.ca <dattu.ca.website@gmail.com>
2023-11-13 15:41:53 -0500

    186449827

--- src/app/(website)/tags/page.tsx
+++ src/app/(website)/tags/page.tsx
@@ -1,5 +1,6 @@
 import {metaTagServices} from "~/services";
 import {MetaTagsListComponent} from "~/app.components/metaTagsListComponent";
+import {redirect} from "next/navigation";
 
 interface IProps {
     params: {}
@@ -10,6 +11,9 @@ export const revalidate = 86400;
 
 const Page = async (props: IProps) => {
     const result = await metaTagServices.fetchAllActiveTags();
+    if (!result || !Array.isArray(result) || result.length === 0) {
+        redirect('/')
+    }
     return <MetaTagsListComponent tags={result}/>
 }
 export default Page;
