e4789e1d3e6dc20648e5ef0ebc8a6e908586503c
dattu.ca <dattu.ca.website@gmail.com>
2023-11-16 18:46:59 -0500

    186409173

--- src/auth.services/options.ts
+++ src/auth.services/options.ts
@@ -24,57 +24,58 @@ export const nextAuthOptions: NextAuthOptions = {
         //     clientSecret: SERVER_CONFIG.GITHUB_AUTH.SECRET,
         // })
     ],
-    // callbacks: {
-    //     signIn: async ({user, account, profile, email, credentials}) => {
-    //         try {
-    //             if (!account?.provider || !account?.providerAccountId) {
-    //                 return false;
-    //             }
-    //
-    //             const userProfile = {
-    //                 name: '',
-    //                 givenName: '',
-    //                 familyName: '',
-    //                 email: '',
-    //             }
-    //             const authProvider = {
-    //                 provider: account?.provider?.toLowerCase(),
-    //                 providerAccountId: account?.providerAccountId,
-    //
-    //             };
-    //
-    //             if (authProvider.provider === 'google') {
-    //                 if (!SERVER_CONFIG.NEXT_AUTH_ALLOWED_IDS.GOOGLE.includes(account?.providerAccountId as string)) {
-    //                     return false;
-    //                 }
-    //                 const p = profile as GoogleProfile;
-    //                 userProfile.name = p.name;
-    //                 userProfile.givenName = p.given_name;
-    //                 userProfile.familyName = p.family_name;
-    //                 userProfile.email = p.email;
-    //             }
-    //             const result = await authDbServices.signIn({ authProvider, userProfile });
-    //             console.log("RESULT", result);
-    //             return Boolean(result);
-    //         } catch (err) {
-    //             console.error('Error in the signin callback', err);
-    //         }
-    //
-    //         return false;
-    //     },
-    //     jwt: async (params) => {
-    //         const {token, account} = params;
-    //         if (account) {
-    //             (token as IToken).authProfileId = await authDbServices.fetchAuthProfileIdFromProviderData(account.provider, account.providerAccountId);
-    //         }
-    //         return token;
-    //     },
-    //     session: async ({session, token, user}) => {
-    //         delete session.user;
-    //         (session as ISession).authProfileId = (token as IToken).authProfileId;
-    //         return session;
-    //     },
-    // },
+    callbacks: {
+        signIn: async ({user, account, profile, email, credentials}) => {
+            try {
+                if (!account?.provider || !account?.providerAccountId) {
+                    return false;
+                }
+
+                const userProfile = {
+                    name: '',
+                    givenName: '',
+                    familyName: '',
+                    email: '',
+                }
+                const authProvider = {
+                    provider: account?.provider?.toLowerCase(),
+                    providerAccountId: account?.providerAccountId,
+
+                };
+
+                if (authProvider.provider === 'google') {
+                    if (!SERVER_CONFIG.NEXT_AUTH_ALLOWED_IDS.GOOGLE.includes(account?.providerAccountId as string)) {
+                        return false;
+                    }
+                    const p = profile as GoogleProfile;
+                    userProfile.name = p.name;
+                    userProfile.givenName = p.given_name;
+                    userProfile.familyName = p.family_name;
+                    userProfile.email = p.email;
+                }
+                return true;
+                // const result = await authDbServices.signIn({ authProvider, userProfile });
+                // console.log("RESULT", result);
+                // return Boolean(result);
+            } catch (err) {
+                console.error('Error in the signin callback', err);
+            }
+
+            return false;
+        },
+        // jwt: async (params) => {
+        //     const {token, account} = params;
+        //     if (account) {
+        //         (token as IToken).authProfileId = await authDbServices.fetchAuthProfileIdFromProviderData(account.provider, account.providerAccountId);
+        //     }
+        //     return token;
+        // },
+        // session: async ({session, token, user}) => {
+        //     delete session.user;
+        //     (session as ISession).authProfileId = (token as IToken).authProfileId;
+        //     return session;
+        // },
+    },
     pages: {
         signIn: '/login',
         error: '/error',
