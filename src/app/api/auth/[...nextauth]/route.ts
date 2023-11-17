660de00dec2d9b79f06e56fa08304a1a19302536
dattu.ca <dattu.ca.website@gmail.com>
2023-11-16 23:54:11 -0500

    186409173

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth';
 import { nextAuthOptions } from '~/auth.services';
 const handler = NextAuth(nextAuthOptions);
 
-export const maxDuration = 60;
 
 export {
     handler as GET,
