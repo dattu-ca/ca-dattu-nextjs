a6f3a0415d6640b5a6e8b4b96aeafe618e3f25e2
d-ca <97037036+dattu-ca@users.noreply.github.com>
2023-11-17 00:14:03 -0500

    186500638 (#161)

--- src/app/(website)/playground/page.tsx
+++ src/app/(website)/playground/page.tsx
@@ -1,13 +1,11 @@
-import {fetchAuthProfileIdFromProviderData} from "~/services.db/auth";
-import {DefaultBlocksLayout} from "~/app.ui.components/blocksLayout/defaultBLocksLayout";
 import clsx from "clsx";
+import {DefaultBlocksLayout} from "~/app.ui.components/blocksLayout/defaultBLocksLayout";
+
 
 const Page = async () => {
-    const authProfile = await fetchAuthProfileIdFromProviderData('google', '110936461031997432633')
-    
     return <DefaultBlocksLayout allFormats='Default'>
         <div className={clsx('mt-8')}>
-        <pre>{JSON.stringify(authProfile, null, 2)}</pre>
+            <h1>Playground</h1>
         </div>
     </DefaultBlocksLayout>
 }
