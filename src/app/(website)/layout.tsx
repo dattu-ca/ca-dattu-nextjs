e573ec84ca3b0ecd0ac2be18506d350da9fcbeab
dattu.ca <dattu.ca.website@gmail.com>
2023-11-16 20:02:25 -0500

    186409173

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -53,6 +53,10 @@ const RootLayout = async ({children}: IProps) => {
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <meta name="robots" content="noindex,nofollow"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="~/assets/favicons/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="~/assets/favicons/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="~/assets/favicons/favicon-16x16.png"/>
+            <link rel="mask-icon" href="~/assets/favicons/safari-pinned-tab.svg" />
         </head>
         <body className={clsx(
             'flex min-h-full ',
